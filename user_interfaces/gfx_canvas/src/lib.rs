88a64e34066fd48757c6ee4fdf15df90b7afd3b6
Andrew Hunter <andrew@logicalshift.co.uk>
2019-11-28 22:24:11 +0000

    Add a new UI library (canvas library based on rust-gfx and lyon)

---
+++ user_interfaces/gfx_canvas/src/lib.rs
@@ -0,0 +1,5 @@
+//!
+//! This provides an interface between a canvas drawing stream from FlowBetween and a gfx back-end
+//! that can be used to render it to a user-interface or to an off-screen buffer
+//!
+
